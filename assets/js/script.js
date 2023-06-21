bdac0d92d384422b147684e9d74f64e1dab62362
Vignesh D <118509275+vigneshvikram0001@users.noreply.github.com>
2023-06-21 18:46:50 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,53 +55,53 @@ function cartClose() {
 
 
 // Adding products in cart
-function finalResult() {
-  let addCart = document.querySelectorAll(".product-add");
-  addCart.forEach((check) => {
-    check.addEventListener("click", (e) => {
-      let findData = e.target.parentElement;
-      let getImgLink = findData.children[1].src;
-      let getCartName = findData.children[2].children[0].innerText;
-      let getCartPrice = findData.children[2].children[1].innerText.substr(0,6);
+// function finalResult() {
+//   let addCart = document.querySelectorAll(".product-add");
+//   addCart.forEach((check) => {
+//     check.addEventListener("click", (e) => {
+//       let findData = e.target.parentElement;
+//       let getImgLink = findData.children[1].src;
+//       let getCartName = findData.children[2].children[0].innerText;
+//       let getCartPrice = findData.children[2].children[1].innerText.substr(0,6);
 
       
-      let orderCartList = document.querySelector(".order-cart-list");
-      let element = result(getImgLink, getCartName, getCartPrice);
-      orderCartList.append(element);
-    });
-  });
+//       let orderCartList = document.querySelector(".order-cart-list");
+//       let element = result(getImgLink, getCartName, getCartPrice);
+//       orderCartList.append(element);
+//     });
+//   });
         
-  function result(getImgLink, getCartName, getCartPrice) {
-  return `<div class="list">
-  <img src="${getImgLink}" class="list-img" />
-  <div class="list-content">
-    <div class="list-content-left">
-      <h3>${getCartName}</h3>
-      <div class="list-buttons">
-        <button>-</button>
-        <input type="text" value="1" />
-        <button>+</button>
-      </div>
-    </div>
-    <div class="list-content-right">
-      <h3 class="cart-price">${getCartPrice}</h3>
-      <button class='trash'><i class="fa fa-trash"></i> Remove</button>
-    </div>
-  </div>
-</div>`
-}
+//   function result(getImgLink, getCartName, getCartPrice) {
+//   return `<div class="list">
+//   <img src="${getImgLink}" class="list-img" />
+//   <div class="list-content">
+//     <div class="list-content-left">
+//       <h3>${getCartName}</h3>
+//       <div class="list-buttons">
+//         <button>-</button>
+//         <input type="text" value="1" />
+//         <button>+</button>
+//       </div>
+//     </div>
+//     <div class="list-content-right">
+//       <h3 class="cart-price">${getCartPrice}</h3>
+//       <button class='trash'><i class="fa fa-trash"></i> Remove</button>
+//     </div>
+//   </div>
+// </div>`
+// }
 
-  // remove list cart from order list
-  const trash = document.querySelectorAll(".trash");
-  trash.forEach((btn) => {
-    btn.addEventListener("click", (event) => {
-      const particularBtn =
-        event.target.parentElement.parentElement.parentElement;
-      console.log(particularBtn);
-      particularBtn.remove();
-    });
-  });
-}
-finalResult();
+//   // remove list cart from order list
+//   const trash = document.querySelectorAll(".trash");
+//   trash.forEach((btn) => {
+//     btn.addEventListener("click", (event) => {
+//       const particularBtn =
+//         event.target.parentElement.parentElement.parentElement;
+//       console.log(particularBtn);
+//       particularBtn.remove();
+//     });
+//   });
+// }
+// finalResult();
 
 
